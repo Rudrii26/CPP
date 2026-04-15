3c30dbf1803cda511c4d7a16d5edaf0caf2b2c99
Rudrii26 <25cs027@charusat.edu.in>
2026-04-15 20:28:16 +0530

    OOP

--- 3.3.cpp
+++ 3.3.cpp
@@ -1,41 +1,108 @@
+//3.3_25CS027
 #include<iostream>
 using namespace std;
+
 class bank{
     int ACNo;
     string Holder;
     int currentBalance;
     static int totalAC;
 
-    public:
-    bank()
-    {
-        int num = 0;
-        string name = "None";
-        int balance = 0;
+public:
+    bank(){
+        ACNo = 0;
+        Holder = "None";
+        currentBalance = 0;
     }
-    bank(int num, string name, int balance)
-    {
-        ACNo = num;
-        Holder = name;
-        currentBalance = balance;
+
+    void input(){
+        cout<<"Enter Account Number: ";
+        cin>>ACNo;
+        cout<<"Enter Holder Name: ";
+        cin>>Holder;
+        cout<<"Enter Balance: ";
+        cin>>currentBalance;
         totalAC++;
     }
+
     void display(){
-        cout<<currentBalance;
+        cout<<"\nAccount No: "<<ACNo;
+        cout<<"\nHolder Name: "<<Holder;
+        cout<<"\nBalance: "<<currentBalance<<endl;
     }
-    void transfer(bank b1[], int num, int amt, int n)
-    {
+
+    static void showTotal(){
+        cout<<"\nTotal Accounts: "<<totalAC<<endl;
+    }
+
+    static void transfer(bank b[], int n, int sender, int receiver, int amt){
+        int sIndex = -1, rIndex = -1;
+
         for(int i=0; i<n; i++){
-            if(b1[i].ACNo == num){
-                if(amt > currentBalance){
-                    cout << "Incefficient Balance!";
-                }
-                else{
-                    b1[i].currentBalance+=amt;
-                    currentBalance-=amt;
-                    display();
-                }
-            }
+            if(b[i].ACNo == sender)
+                sIndex = i;
+            if(b[i].ACNo == receiver)
+                rIndex = i;
         }
+
+        if(sIndex == -1 || rIndex == -1){
+            cout<<"Invalid Account Number!\n";
+            return;
+        }
+
+        if(sender == receiver){
+            cout<<"Cannot transfer to same account!\n";
+            return;
+        }
+
+        if(b[sIndex].currentBalance < amt){
+            cout<<"Insufficient Balance!\n";
+            return;
+        }
+
+        b[sIndex].currentBalance -= amt;
+        b[rIndex].currentBalance += amt;
+
+        cout<<"\nTransfer Successful!\n";
     }
 };
+
+int bank::totalAC = 0;
+
+int main(){
+    int n;
+    cout<<"Enter number of accounts: ";
+    cin>>n;
+
+    bank *b = new bank[n];
+
+    for(int i=0; i<n; i++){
+        cout<<"\nEnter details for account "<<i+1<<endl;
+        b[i].input();
+    }
+
+    cout<<"\n--- Before Transfer ---\n";
+    for(int i=0; i<n; i++){
+        b[i].display();
+    }
+
+    int s, r, amt;
+    cout<<"\nEnter Sender AC No: ";
+    cin>>s;
+    cout<<"Enter Receiver AC No: ";
+    cin>>r;
+    cout<<"Enter Amount: ";
+    cin>>amt;
+
+    bank::transfer(b, n, s, r, amt);
+
+    cout<<"\n--- After Transfer ---\n";
+    for(int i=0; i<n; i++){
+        b[i].display();
+    }
+
+    bank::showTotal();
+
+    delete[] b;
+    return 0;
+}
