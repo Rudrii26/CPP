a47eb21aee1f7a472dd701462631f48b33bde4f9
Rudrii26 <25cs027@charusat.edu.in>
2026-04-15 20:26:04 +0530

    OOP

--- 3.2.cpp
+++ 3.2.cpp
@@ -1,4 +1,4 @@
-//3.2
+//3.2_25CS027
 #include<iostream>
 using namespace std;
 
@@ -47,68 +47,3 @@ int main() {
     return 0;
 }
 
-
-
-// #include<iostream>
-// using namespace std;
-// class sum{
-//     int total = 0;
-//     int max = 0;
-//     public:
-//     int recursive(int a[], int x)
-//     {
-//         if(x == 0){
-//             return 0;
-//         }
-//         return a[x-1] + recursive(a, x-1);
-//     }
-
-//     int Max(int a[], int x)
-//     {
-//         if(x == 1)
-//         {
-//             return a[0];
-//         }
-//         int m = Max(a , x - 1);
-
-//         if(a[x-1] > m)
-//             return a[x-1];
-//         else
-//             return m;
-//     }
-
-//     int iterative(int a[], int x)
-//     {
-//         for(int i=0; i<x; i++)
-//         {
-//             total+=a[i];
-//         }
-//         cout<<"Total: "<<total<<endl;
-
-//         for(int j=0; j<x; j++){
-//             if(a[j]>max){
-//                 max = a[j];
-//             }
-//         }
-//         cout<< "Max using iterative: "<<max<<endl;
-//         return total;
-//     }
-// };
-// int main()
-// {
-//     int n;
-//     sum a;
-//     cout<<"Enter the Number of elements: ";
-//     cin>>n;
-
-//     int *s = (int *)malloc(sizeof(int)*n);
-//     for(int i=0; i<n; i++)
-//     {
-//         cout<<"Enter "<<i+1 <<"th Element: ";
-//         cin>>s[i];;
-//     }
-//     a.iterative(s, n);
-//     cout<<"Recursion: "<<a.recursive(s, n); 
-//     cout<<endl;
-//     cout<<"Maximum element using Recursion: "<<a.Max(s, n);                                                                                                       
-// }
